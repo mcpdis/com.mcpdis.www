179cd834bcdefbc11d0fbb58e3ca455e47a94499
Cyril David <cyx.ucron@gmail.com>
2012-02-14 02:55:02 +0800

    Port the send wap header thing.

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -2,10 +2,14 @@ module MCPDIS
   module Helpers
     def current_user
       authenticated(User)
-    end  
+    end
 
     def has_error?(model, att)
       model.errors[att].any?
+    end
+
+    def send_wap_header!
+      headers["Content-Type"] = "application/xhtml+xml"
     end
   end
 end
