d01698360f3a5ab073dd52ddc49264e48430fdd8
Cyril David <cyx.ucron@gmail.com>
2012-02-14 02:55:38 +0800

    Separate out the user and create_user models.

--- lib/user.rb
+++ lib/user.rb
@@ -1,31 +1,3 @@
-class CreateUser < Scrivener
-  attr_accessor :email
-  attr_accessor :password
-  attr_accessor :password_confirmation
-  attr_accessor :first_name
-  attr_accessor :last_name
-
-  def validate
-    assert_email(:email)
-    assert_present(:first_name)
-    assert_present(:last_name)
-
-    if assert_present(:password)
-      assert_length(:password, 6..1000)
-      assert_confirmed(:password)
-    end
-  end
-
-  def create
-    User.create(attributes.reject { |att, _| att == :password_confirmation })
-  end
-
-private
-  def assert_confirmed(att1, att2 = :"#{att1}_confirmation", err = [att2, :not_valid])
-    assert send(att1) == send(att2), err
-  end
-end
-
 class User < Ohm::Model
   extend Shield::Model
 
