21877b3c0d34209ed074c0c3c4ae045cf310b27e
Cyril David <cyx@pipetodevnull.com>
2012-02-24 18:56:02 +0800

    Add a couple of convenience helpers.

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -5,11 +5,24 @@ module MCPDIS
     end
 
     def has_error?(model, att)
-      model.errors[att].any?
+      model.errors.has_key?(att)
     end
 
     def send_wap_header!
       headers["Content-Type"] = "application/xhtml+xml"
     end
+
+    def mote_vars(content)
+      super.merge(path: req.path)
+    end
+
+    def under?(fragment, path)
+      path.start_with?(fragment)
+    end
+
+    def in?(fragment, path)
+      path == fragment
+    end
+
   end
 end
