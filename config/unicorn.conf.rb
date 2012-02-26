e16d685cf8752bc95f3fa27a4ac25c9a0fb3607e
Cyril David <cyx@pipetodevnull.com>
2012-02-27 04:46:16 +0800

    Self managing unicorn.

--- config/unicorn.conf.rb
+++ config/unicorn.conf.rb
@@ -4,3 +4,20 @@ user "cyx", "cyx"
 pid "/var/run/unicorn.pid"
 stderr_path "/var/log/unicorn/stderr.log"
 stdout_path "/var/log/unicorn/stdout.log"
+
+before_fork do |server, worker|
+  # This allows a new master process to incrementally
+  # phase out the old master process with SIGTTOU to avoid a
+  # thundering herd (especially in the "preload_app false" case)
+  # when doing a transparent upgrade.  The last worker spawned
+  # will then kill off the old master process with a SIGQUIT.
+  old_pid = "#{server.config[:pid]}.oldbin"
+
+  if old_pid != server.pid
+    begin
+      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+      Process.kill(sig, File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+    end
+  end
+end
